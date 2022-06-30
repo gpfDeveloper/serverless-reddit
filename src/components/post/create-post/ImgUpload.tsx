baec235e0be6b774625d1250645bb7a1c4e2d3d8
Pengfei Gao <gpfdeveloper@gmail.com>
2022-06-30 21:05:03 +0800

    upload UX enhance

--- src/components/post/create-post/ImgUpload.tsx
+++ src/components/post/create-post/ImgUpload.tsx
@@ -10,6 +10,7 @@ type Props = {
 
 const ImgUpload: FunctionComponent<Props> = ({ img, setImg }) => {
   const [previewUrl, setPreviewUrl] = useState('');
+  const [canDrop, setCanDrop] = useState(false);
 
   const createPreview = (files: FileList) => {
     const img = files[0];
@@ -26,6 +27,12 @@ const ImgUpload: FunctionComponent<Props> = ({ img, setImg }) => {
   const dragHandler = (e: React.DragEvent) => {
     e.preventDefault();
     e.stopPropagation();
+    if (e.type === 'dragover') {
+      setCanDrop(true);
+    }
+    if (e.type === 'dragleave') {
+      setCanDrop(false);
+    }
   };
 
   const dropHandler = (e: React.DragEvent) => {
@@ -39,6 +46,7 @@ const ImgUpload: FunctionComponent<Props> = ({ img, setImg }) => {
   const removeImgHandler = () => {
     setImg(undefined);
     setPreviewUrl('');
+    setCanDrop(false);
   };
   return (
     <Box
@@ -59,32 +67,54 @@ const ImgUpload: FunctionComponent<Props> = ({ img, setImg }) => {
             gap: 2,
             justifyContent: 'center',
             alignItems: 'center',
-            border: '1px dashed #343536',
+            borderStyle: 'dashed',
+            borderWidth: canDrop ? '3px' : '1px',
+            borderColor: canDrop ? 'grey.400' : 'grey.800',
           }}
           onDragEnter={dragHandler}
           onDragOver={dragHandler}
+          onDragLeave={dragHandler}
           onDrop={dropHandler}
         >
-          <Typography color="text.secondary" variant="h6">
-            Drag and drop image or
-          </Typography>
-          <label htmlFor="upload-img">
-            <input
-              onChange={previewHandler}
-              id="upload-img"
-              accept="image/*"
-              style={{ display: 'none' }}
-              type="file"
-            />
-            <Button
-              component="span"
-              variant="outlined"
-              size="large"
-              color="secondary"
+          {!canDrop && (
+            <>
+              <Typography color="text.secondary" variant="h6">
+                Drag and drop image or
+              </Typography>
+              <label htmlFor="upload-img">
+                <input
+                  onChange={previewHandler}
+                  id="upload-img"
+                  accept="image/*"
+                  style={{ display: 'none' }}
+                  type="file"
+                />
+                <Button
+                  component="span"
+                  variant="outlined"
+                  size="large"
+                  color="secondary"
+                >
+                  Upload
+                </Button>
+              </label>
+            </>
+          )}
+          {canDrop && (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
             >
-              Upload
-            </Button>
-          </label>
+              <Box sx={{ height: 100, width: 100 }}>
+                <img src="/hamster.png" alt="hamster" />
+              </Box>
+              <Typography variant="h6">Drop here to upload</Typography>
+            </Box>
+          )}
         </Box>
       )}
       {img && (
