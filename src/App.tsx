67f65ba0643bcaf6ba3c29b34be144c8cfb7009e
Pengfei Gao <gpfdeveloper@gmail.com>
2022-07-01 10:56:37 +0800

    signup UI refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import { useAuth } from './context/auth-context';
-import SignupConfirm from './pages/SignupConfirm';
 import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
       <Route path="/post/:id" element={<PostDetail />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/signup-confirm" element={<SignupConfirm />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="*" element={<Navigate replace to="/login" />} />
     </Routes>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -7,17 +7,16 @@ import {
   TextField,
 } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import GoogleIcon from '@mui/icons-material/Google';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import { Controller, useForm } from 'react-hook-form';
 import { BankLayout } from '../components/layout/BankLayout';
 import { LINK_STYLE } from '../theme/theme';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/auth-context';
 
 const Signup: FunctionComponent = () => {
-  const navigate = useNavigate();
+  const [isConfirm, setIsConfirm] = useState(false);
   const { login } = useAuth();
   const {
     formState: { errors },
@@ -25,7 +24,10 @@ const Signup: FunctionComponent = () => {
     control,
   } = useForm();
   const signupHandler = () => {
-    navigate('/signup-confirm');
+    setIsConfirm(true);
+  };
+  const confirmSignupHandler = () => {
+    login();
   };
   const signupGoogleHandler = () => {
     login();
@@ -33,6 +35,178 @@ const Signup: FunctionComponent = () => {
   const signupFacebookHandler = () => {
     login();
   };
+
+  let content = (
+    <>
+      <Typography variant="h6" fontWeight={700}>
+        Sign Up
+      </Typography>
+      <Divider />
+      <Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+          }}
+        >
+          <Button
+            onClick={signupGoogleHandler}
+            variant="contained"
+            fullWidth
+            size="large"
+            startIcon={<GoogleIcon />}
+          >
+            Continue with Google
+          </Button>
+          <Button
+            onClick={signupFacebookHandler}
+            variant="contained"
+            fullWidth
+            size="large"
+            startIcon={<FacebookIcon />}
+          >
+            Continue with Facebook
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <Box sx={{ backgroundColor: '#343536', height: 1.2, flex: 1 }}></Box>
+        <Typography fontWeight={700} color="text.disabled">
+          OR
+        </Typography>
+        <Box sx={{ backgroundColor: '#343536', height: 1.2, flex: 1 }}></Box>
+      </Box>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(signupHandler)}
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+        }}
+      >
+        <Controller
+          name="username"
+          defaultValue=""
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <TextField
+              color="secondary"
+              error={Boolean(errors.username)}
+              helperText={errors.username && 'Please enter your name'}
+              fullWidth
+              label="User Name"
+              {...field}
+            />
+          )}
+        />
+        <Controller
+          name="email"
+          defaultValue=""
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <TextField
+              type="email"
+              color="secondary"
+              error={Boolean(errors.email)}
+              helperText={errors.email && 'Please enter your email'}
+              fullWidth
+              label="Email Address"
+              {...field}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          defaultValue=""
+          control={control}
+          rules={{ required: true, minLength: 6 }}
+          render={({ field }) => (
+            <TextField
+              type="password"
+              color="secondary"
+              error={Boolean(errors.password)}
+              helperText={
+                errors.password && 'Password should more than 6 charactor'
+              }
+              fullWidth
+              label="Password"
+              {...field}
+            />
+          )}
+        />
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          size="large"
+          color="secondary"
+        >
+          Sign Up
+        </Button>
+        <Box sx={LINK_STYLE}>
+          {'Aready have an account?'} &nbsp;
+          <RouterLink to="/login">Log In</RouterLink>
+        </Box>
+      </Box>
+    </>
+  );
+
+  if (isConfirm) {
+    content = (
+      <>
+        <Typography variant="h6" fontWeight={700}>
+          Confirm your account
+        </Typography>
+        <Divider />
+
+        <Box
+          component="form"
+          onSubmit={handleSubmit(confirmSignupHandler)}
+          sx={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+          }}
+        >
+          <Controller
+            name="code"
+            defaultValue=""
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextField
+                color="secondary"
+                error={Boolean(errors.code)}
+                helperText={errors.code && 'Please enter confirmation code'}
+                fullWidth
+                label="Confirmation Code"
+                {...field}
+              />
+            )}
+          />
+
+          <Button type="submit" fullWidth variant="contained" size="large">
+            Sign Up
+          </Button>
+          <Button
+            color="secondary"
+            sx={{ alignSelf: 'flex-start' }}
+            onClick={() => setIsConfirm(false)}
+          >
+            Back
+          </Button>
+        </Box>
+      </>
+    );
+  }
+
   return (
     <BankLayout>
       <Paper
@@ -44,122 +218,7 @@ const Signup: FunctionComponent = () => {
           width: { xs: '100%', sm: 600 },
         }}
       >
-        <Typography variant="h6" fontWeight={700}>
-          Sign Up
-        </Typography>
-        <Divider />
-        <Box>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 2,
-            }}
-          >
-            <Button
-              onClick={signupGoogleHandler}
-              variant="contained"
-              fullWidth
-              size="large"
-              startIcon={<GoogleIcon />}
-            >
-              Continue with Google
-            </Button>
-            <Button
-              onClick={signupFacebookHandler}
-              variant="contained"
-              fullWidth
-              size="large"
-              startIcon={<FacebookIcon />}
-            >
-              Continue with Facebook
-            </Button>
-          </Box>
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <Box sx={{ backgroundColor: '#343536', height: 1.2, flex: 1 }}></Box>
-          <Typography fontWeight={700} color="text.disabled">
-            OR
-          </Typography>
-          <Box sx={{ backgroundColor: '#343536', height: 1.2, flex: 1 }}></Box>
-        </Box>
-        <Box
-          component="form"
-          onSubmit={handleSubmit(signupHandler)}
-          sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 2,
-          }}
-        >
-          <Controller
-            name="username"
-            defaultValue=""
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                color="secondary"
-                error={Boolean(errors.username)}
-                helperText={errors.username && 'Please enter your name'}
-                fullWidth
-                label="User Name"
-                {...field}
-              />
-            )}
-          />
-          <Controller
-            name="email"
-            defaultValue=""
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                type="email"
-                color="secondary"
-                error={Boolean(errors.email)}
-                helperText={errors.email && 'Please enter your email'}
-                fullWidth
-                label="Email Address"
-                {...field}
-              />
-            )}
-          />
-          <Controller
-            name="password"
-            defaultValue=""
-            control={control}
-            rules={{ required: true, minLength: 6 }}
-            render={({ field }) => (
-              <TextField
-                type="password"
-                color="secondary"
-                error={Boolean(errors.password)}
-                helperText={
-                  errors.password && 'Password should more than 6 charactor'
-                }
-                fullWidth
-                label="Password"
-                {...field}
-              />
-            )}
-          />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            size="large"
-            color="secondary"
-          >
-            Sign Up
-          </Button>
-          <Box sx={LINK_STYLE}>
-            {'Aready have an account?'} &nbsp;
-            <RouterLink to="/login">Log In</RouterLink>
-          </Box>
-        </Box>
+        {content}
       </Paper>
     </BankLayout>
   );
